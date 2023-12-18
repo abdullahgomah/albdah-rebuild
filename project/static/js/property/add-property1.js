0223541c0e421a37996f377b47fc1e5c70facfaa
Abdullah Mohammed <djangodeveloper6@gmail.com>
2023-12-18 03:58:57 +0200

    edit rent_Type box

--- project/static/js/property/add-property1.js
+++ project/static/js/property/add-property1.js
@@ -891,4 +891,6 @@ document.querySelector('form').onsubmit = function () {
     // var description = `{% autoescape off %}{{ property.description }}{% endautoescape %}`; 
     var description = document.querySelector('[name=property__description__input]').value; 
     document.querySelector('[name=property__description__input]').value = marked(description); 
+    alert(document.querySelector('[name=property__description__input]').value)
+
 }
