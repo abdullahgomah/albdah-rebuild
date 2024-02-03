25bbb4f79307552547123f4265540f03122ee14f
abdullahgomah <djangodeveloper6@gmail.com>
2024-02-03 10:52:07 -0800

    EDIT

--- page/models.py
+++ page/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Home(models.Model): 
     title = models.CharField(max_length=200, verbose_name="العنوان الرئيسي") 
     phone = models.CharField(max_length=20, verbose_name="رقم الجوال") 
-    slogan = models.TextField(verbose_name="الجملة الفرعية")
+    slogan = models.TextField(verbose_name="الجملة الفرعية", null=True, blank=True)
     snapchat = models.TextField(verbose_name="رابط سناب شات", null=True, blank=True) 
     whatsapp = models.TextField(verbose_name="رابط واتساب", null=True, blank=True) 
     tiktok = models.TextField(verbose_name="رابط تيكتوك", null=True, blank=True) 
