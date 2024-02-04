00a12d124406acbfbacc745e0c1cc9783cb816d5
abdullahgomah <djangodeveloper6@gmail.com>
2024-02-04 04:03:23 +0300

    edit

---
+++ static/js/pages/index.0089c9fe979f.js
@@ -0,0 +1,35 @@
+let propertyTypeBtns = document.querySelectorAll('.swiper-slide .btn') 
+let allProperties = document.querySelectorAll('.item.property') 
+
+
+propertyTypeBtns.forEach((btn) => {
+    // console.log(btn.dataset.property_type)
+    btn.addEventListener('click', () => {
+        if (btn.dataset.property_type == 'all') {
+            allProperties.forEach((ad) => {
+                ad.style.display = 'flex';
+            })
+        } else {
+
+            
+            allProperties.forEach((ad) => {
+                ad.style.display = 'flex';
+            })
+            allProperties.forEach((property) => {
+                if (property.querySelector('[name="property_type_input"]').value != btn.dataset.property_type) {
+                    property.style.display = 'none';
+                } 
+            })
+        }
+    })
+})
+
+
+let propertyCoverImg = document.querySelectorAll('.property__cover_img img'); 
+
+propertyCoverImg.forEach((img) => {
+    if (img.src == window.location.href) {
+        img.src = img.parentElement.parentElement.querySelector("[name=propertyFirstImgUrl]").value
+    }
+})
+
