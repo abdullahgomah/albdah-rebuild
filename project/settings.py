95c3b79347e1401c19b691af81ae5d2dd39be415
abdullahgomah <djangodeveloper6@gmail.com>
2024-02-07 01:13:02 +0300

    edit password reset

--- project/settings.py
+++ project/settings.py
@@ -163,3 +163,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CSRF_TRUSTED_ORIGINS=["https://www.albdah.sa", "https://albdah.sa"]
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER= 'djangodeveloper6@gmail.com'
+EMAIL_HOST_PASSWORD = "qawsedrevyzcguqq"
