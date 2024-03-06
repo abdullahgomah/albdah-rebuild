8e46dfc5f2e9ffc0946ecac9d8a5379ea4d986cb
Abdullah Mohammed <djangodeveloper6@gmail.com>
2024-03-06 18:50:51 +0200

    edit

--- project/static/js/pages/app.js
+++ project/static/js/pages/app.js
@@ -1,21 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
-    freeMode: true,
     slidesPerView: 1,
     loop: true, 
     spaceBetween: 30, 
-    minimumVelocity: 1, 
-    momentum: false, 
-    sticky: true, 
-    breakpoints: {
-      320: {
-        slidesPerView: 4,
-        spaceBetween: 20
-      },
-      640: {
-        slidesPerView: 7,
-        spaceBetween: 30
-      }
-    }
+
     /*pagination: {
       el: ".swiper-pagination",
       clickable: true,
