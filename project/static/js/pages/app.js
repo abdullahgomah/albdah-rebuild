0956e66c3d3002e8b47b41c465394d2baf20d656
Abdullah Mohammed <djangodeveloper6@gmail.com>
2024-03-06 17:16:38 +0200

    fix: try new solution for slider

--- project/static/js/pages/app.js
+++ project/static/js/pages/app.js
@@ -1,7 +1,9 @@
 var swiper = new Swiper(".mySwiper", {
-    // freeMode: true,
-    slidesPerView: 7,
-    spaceBetween: 10, 
+    freeMode: true,
+    slidesPerView: 1,
+    loop: true, 
+    spaceBetween: 1, 
+    allowSlideNext: false, 
     /*pagination: {
       el: ".swiper-pagination",
       clickable: true,
