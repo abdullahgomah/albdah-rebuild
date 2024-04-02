5a4135ba8945ffc84f824612081d087e04c3bc5e
Abdullah Mohammed <djangodeveloper6@gmail.com>
2024-04-03 01:06:15 +0200

    try: create features to get client ip

--- page/views.py
+++ page/views.py
@@ -8,6 +8,17 @@ from .models import *
 
 
 def index(request): 
+
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    print('ip-==========-')
+    print(ip) 
+
+    print('--------')
     
     v = Visitor.objects.first()
     v.home +=1 
