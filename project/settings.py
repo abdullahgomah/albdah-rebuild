cd38d3a68d8bf7a72ced8e950252e517db3b907b
abdullahgomah <djangodeveloper6@gmail.com>
2024-08-21 02:45:10 +0300

    Edit

--- project/settings.py
+++ project/settings.py
@@ -60,20 +60,20 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 
 ]
 
 
 CORS_ALLOWED_ORIGINS = [
     "https://albdah.sa",
-    "http://127.0.0.1:8001",
+    "http://127.0.0.1:8001",    
 ]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
