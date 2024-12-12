97cccb8b29a2ce5e9c697cee91a239bc6747f36d
Abdullah Mohammed <djangodeveloper6@gmail.com>
2024-12-12 17:51:28 +0200

    feat: add timeout to database options

--- project/settings.py
+++ project/settings.py
@@ -124,7 +124,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
         "OPTIONS": {
             # ...
-            "timeout": 20,
+            "timeout": 1000,
             # ...
         }
     }
